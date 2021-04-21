0f5088dad7f6dee9f60b1bd8e74155849507bee2
FRYoussef <f.r.youssef@hotmail.com>
2021-04-21 10:44:42 +0200

    fixed hardcoded mqtt uris

--- src/mqtt.h
+++ src/mqtt.h
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "mqtt_client.h"
 #include "esp_log.h"
 #include "sensors_readings.h"
@@ -7,11 +8,25 @@ static esp_mqtt_client_handle_t client;
 
 
 bool first_conexion = true;
-                                                    // /location/board name/sensor metric/sensor number/config parameter  
-static const char * TOPIC_SAMPLE_FREQ_IRRADIATION = "/ciu/lopy4/irradiation/1/sample_frequency";
-static const char * TOPIC_SEND_FREQ_IRRADIATION = "/ciu/lopy4/irradiation/1/send_frequency";
-static const char * TOPIC_N_SAMPLES_IRRADIATION = "/ciu/lopy4/irradiation/1/sample_number";
 
-static const char * TOPIC_SAMPLE_FREQ_BATTERY_LEVEL = "/ciu/lopy4/battery_level/1/sample_frequency";
-static const char * TOPIC_SEND_FREQ_BATTERY_LEVEL = "/ciu/lopy4/battery_level/1/send_frequency";
-static const char * TOPIC_N_SAMPLES_BATTERY_LEVEL = "/ciu/lopy4/battery_level/1/sample_number";
+// /location/board name/sensor metric/sensor number/config parameter
+const char *MQTT_TOPIC_IRRAD = CONFIG_MQTT_TOPIC_IRRAD;
+const char *MQTT_TOPIC_BATTERY = CONFIG_MQTT_TOPIC_BATTERY;
+
+static const char * TOPIC_SAMPLE_FREQ_IRRADIATION;
+sprintf(TOPIC_SAMPLE_FREQ_IRRADIATION, "%s/sample_frequency", MQTT_TOPIC_IRRAD);
+
+static const char * TOPIC_SEND_FREQ_IRRADIATION;
+sprintf(TOPIC_SEND_FREQ_IRRADIATION, "%s/send_frequency", MQTT_TOPIC_IRRAD);
+
+static const char * TOPIC_N_SAMPLES_IRRADIATION;
+sprintf(TOPIC_N_SAMPLES_IRRADIATION, "%s/sample_number", MQTT_TOPIC_IRRAD);
+
+static char * TOPIC_SAMPLE_FREQ_BATTERY_LEVEL;
+sprintf(TOPIC_SAMPLE_FREQ_BATTERY_LEVEL, "%s/sample_frequency", MQTT_TOPIC_BATTERY);
+
+static char * TOPIC_SEND_FREQ_BATTERY_LEVEL;
+sprintf(TOPIC_SEND_FREQ_BATTERY_LEVEL, "%s/send_frequency", MQTT_TOPIC_BATTERY);
+
+static char * TOPIC_N_SAMPLES_BATTERY_LEVEL;
+sprintf(TOPIC_N_SAMPLES_BATTERY_LEVEL, "%s/sample_number", MQTT_TOPIC_BATTERY);
